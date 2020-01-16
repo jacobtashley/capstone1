cbc5e291aabfdf2d6eab8c9bca35a9691da94d0b
Jacob Ashley <jacobashley@Jacobs-Mac-Pro.local>
2020-01-15 23:45:27 -0800

    updated 1.15.20

--- script.js
+++ script.js
@@ -1,21 +1,107 @@
 'use strict'
 
-$(watchForm());
-
-const searchUrl = 'https://cors-anywhere.herokuapp.com/http://www.giantbomb.com/api/search/?api_key=a6341e2763bab65b72518f7d85807ecba870afae&format=json&query="donkey+kong"&field_list=name,platforms&resource_type=game';
+watchForm();
 
 function watchForm(){
     $('form').submit(event => {
     event.preventDefault();
     getGameInfo();
+    getTubeInfo();
+    // getWikiInfo();
     })
 }
 
 function getGameInfo(){
-    
+    var inputVal = $(".searchBox").val();
+
+    const searchUrl = `https://cors-anywhere.herokuapp.com/http://www.giantbomb.com/api/search/?api_key=a6341e2763bab65b72518f7d85807ecba870afae&format=json&query=${inputVal}&field_list=name,platforms,image,description&resource_type=game`;
+
     fetch(searchUrl)
     .then(response => response.json())
     .then(newResponse => {
         console.log(newResponse);
+        
+        return newResponse;
     })
+    .then(newResponse => displayResults(newResponse, inputVal))
 }
+
+function displayResults(newResponse) {
+    
+    let filteredResults = newResponse.results.filter(
+        item =>
+          item.platforms &&
+          item.platforms.find(platform => platform.name === "Arcade")
+      );
+      console.log(filteredResults);
+    
+    $('#results-list').empty();
+
+    for(let i = 0; i < filteredResults.length; i++) {
+        $('#results-list').append(
+            `<li>
+            <h2>${filteredResults[i].name}</h2>
+            <input type="image" src="${filteredResults[i].image.small_url}">
+            <h3>${filteredResults[i].description}</h3>
+            </li>`)
+    }
+    $('#results').removeClass('hidden')
+}
+
+function getTubeInfo(){
+    var inputVal = $(".searchBox").val();
+
+    const searchUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q="${inputVal}+Arcade+Gameplay"&maxResults=3&key=AIzaSyBS0DdV2r80IS0_n1RsSpn2NTP2NP_xDBQ`;
+
+    fetch(searchUrl)
+    .then(response => response.json())
+    .then(tubeResponse => {
+        console.log(tubeResponse);
+        
+        return tubeResponse;
+    })
+    .then(tubeResponse => displayTube(tubeResponse, inputVal))
+}
+
+function displayTube(tubeResponse){
+    $('#results-list2').empty();
+    
+    for(let i = 0; i < tubeResponse.items.length; i++) {
+        $('#results-list2').append(
+            `<li>
+            <a href="https://www.youtube.com/watch?v=${tubeResponse.items[i].id.videoId}" target="_blank"><h3>${tubeResponse.items[i].snippet.title}</h3></a>
+            <a href="https://www.youtube.com/watch?v=${tubeResponse.items[i].id.videoId}" target="_blank"><img src='${tubeResponse.items[i].snippet.thumbnails.default.url}'></a>
+            </li>`)
+    }
+    $('#tubeResults').removeClass('hidden')
+
+}
+
+// function getWikiInfo(){
+//     var inputVal = $(".searchBox").val();
+
+//     // const searchUrl = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=Crazy Kong`;
+//     const searchUrl = `https://cors-anywhere.herokuapp.com/https://serpapi.com/search?q=Donkey+Kong&tbm=isch&ijn=0`;
+
+//     fetch(searchUrl)
+//     .then(response => response.json())
+//     .then(wikiResponse => {
+//         console.log(wikiResponse);
+        
+//         return wikiResponse;
+//     })
+//     .then(wikiResponse => displayWiki(wikiResponse, inputVal))
+// }
+
+// function displayWiki(wikiResponse){
+//     $('#results-list3').empty();
+//     console.log(wikiResponse)
+//     $('#results-list3').append(
+//         `<li>
+//         <a href="${wikiResponse[3][0]}" target="_blank"><h2>${wikiResponse[1][0]}</h2>
+//         </li>`
+//     )
+    
+//     $('#wikiResults').removeClass('hidden')
+// };
+    
