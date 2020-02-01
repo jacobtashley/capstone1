111ab12c04713b33589a0ad565066eb870fb4d64
Jacob Ashley <jacobashley@Jacobs-Mac-Pro.local>
2020-02-01 10:46:19 -0800

    update 1.1

--- script.js
+++ script.js
@@ -2,84 +2,117 @@
 
 watchForm();
 
-$(document).scroll(function() {
-    var y = $(this).scrollTop();
-    if (y > 425) {
-        $('.arrow').removeClass('hidden')
-        $('#arrowImage').fadeIn();
-    } else {
-      $('#arrowImage').fadeOut();
-    }
-});
+$('.infoContainer').hide();
+$('.tubeContainer').hide();
 
-function watchForm(){
+function watchForm() {
+
     $('form').submit(event => {
-    event.preventDefault();
-    getGameInfo(getTubeInfo);
+    event.preventDefault()
+    getGameInfo()
     })
 }
 
-function getGameInfo(cb){
+function getGameInfo() {
     var inputVal = $(".searchBox").val();
 
     const searchUrl = `https://cors-anywhere.herokuapp.com/http://www.giantbomb.com/api/search/?api_key=a6341e2763bab65b72518f7d85807ecba870afae&format=json&query=${inputVal}&field_list=name,platforms,image,description&resource_type=game`;
 
+    if(!inputVal){
+        return;
+    }
+
+    $('.errorMessage').hide()
+    $('.tubeResults').hide();
+    $('.tubeContainer').hide();
+    $('.infoContainer').fadeOut(500);
+    $('.infoResults').fadeOut(500);
+    $('.loadingMessage').show()
+    $('.loadingMessage').text('Searching...')
+
+   
+
     fetch(searchUrl)
     .then(response => response.json())
     .then(newResponse => {
-      displayResults(newResponse, inputVal);
-      cb();
-    });
-    
+
+        let filteredResults = newResponse.results.filter(
+            item =>
+              item.platforms &&
+              item.platforms.find(platform => platform.name === "Arcade")
+        );
+        
+        if(filteredResults.length > 0) {
+            $('.loading').hide()
+            $('.errorMessage').hide()
+            displayResults(filteredResults)
+            getTubeInfo()
+        
+        } else {
+            $('.loadingMessage').hide()
+            $('.errorMessage').show()
+            $('.errorMessage').text('No information on this game. Please try again.')
+        };
+        
+    })
 }
 
-function displayResults(newResponse) {
+function displayResults(filteredResults) {
     
-    let filteredResults = newResponse.results.filter(
-        item =>
-          item.platforms &&
-          item.platforms.find(platform => platform.name === "Arcade")
-      );
-    
-    $('#infoResults').empty();
+    $('.infoResults').empty();
 
     for(let i = 0; i < filteredResults.length; i++) {
-        $('#infoResults').append(
+        $('.infoResults').append(
             `
-            <input id="imageOne" type="image" src="${filteredResults[i].image.small_url}">
-            <p>${filteredResults[i].description}</p>
-            <p>All information being pulled from www.giantbomb.com<p>
+            <input class="imageOne" type="image" src="${filteredResults[i].image.small_url}">
+            <div class="description">
+                <p>${filteredResults[i].description}</p>
+            </div>
+            <div class="credit">
+                <p>All information being pulled from www.giantbomb.com<p>
+            </div>
             `
         )
     }
-    // $('#infoResults').removeClass('hidden')
-    // $('.infoContainer').removeClass('hidden')
+
     $('.infoContainer').fadeIn(1500);
-    $('#infoResults').fadeIn(1500);
+    $('.infoResults').fadeIn(1500);
 }
 
 
-function getTubeInfo(cb){
-    var inputVal = $(".searchBox").val();
+function getTubeInfo() {
+    var inputVal = $('.searchBox').val();
 
     const searchUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q="${inputVal}+Arcade+Gameplay"&maxResults=3&key=AIzaSyBS0DdV2r80IS0_n1RsSpn2NTP2NP_xDBQ`;
 
     fetch(searchUrl)
     .then(response => response.json())
     .then(tubeResponse => displayTube(tubeResponse, inputVal))
-    cb()
 }
 
 function displayTube(tubeResponse){
-    $('#results-list2').empty();
+    $('.tubeList').empty();
     
     for(let i = 0; i < tubeResponse.items.length; i++) {
-        $('#results-list2').append(
+        $('.tubeList').append(
             `
             <a href="https://www.youtube.com/watch?v=${tubeResponse.items[i].id.videoId}" target="_blank"><h3>${tubeResponse.items[i].snippet.title}</h3></a>
-            <a href="https://www.youtube.com/watch?v=${tubeResponse.items[i].id.videoId}" target="_blank"><img id="youTubeImages" src='${tubeResponse.items[i].snippet.thumbnails.medium.url}'></a>
-            `)
+            <a href="https://www.youtube.com/watch?v=${tubeResponse.items[i].id.videoId}" target="_blank"><img class="youTubeImages" src="${tubeResponse.items[i].snippet.thumbnails.medium.url}" alt="Image Of Video"></a>
+            `
+        )
     }
-    $('#tubeResults').fadeIn(3000);
+    
+    $('.tubeResults').fadeIn(3000);
     $('.tubeContainer').fadeIn(3000);
 }
+
+//Arrow "back to top" function//
+$(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 425) {
+        $('.arrow').removeClass('hidden')
+        $('.arrowImage').fadeIn();
+    } else {
+      $('.arrowImage').fadeOut();
+    }
+});
